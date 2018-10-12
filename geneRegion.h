9044d5c8448ee831aa2f18d970d7c8e7417989e6
ttbond <xu_tun@qq.com>
2018-10-12 16:23:12 +0800

    fix geneRegion.h

--- geneRegion.h
+++ geneRegion.h
@@ -2,26 +2,17 @@
 // Created by ttbond on 18-10-12.
 //
 
-#ifndef SRCFIND_GENEREGION_H
-#define SRCFIND_GENEREGION_H
-//
-// Created by ttbond on 18-10-9.
-//
-
-#include "geneRegion.h"
-
 class geneRegion{
 public:
     geneRegion(int init);
     geneRegion(char *source,char *_agct);
+    void addRegion(char *source);
+    void addRegion(int _chr,long long _st,long long _ed);
+    void solAndPrintRel(FILE *fp);
+
     static int geneRegion::firstGeneRegion;
     static int geneRegion::refChr;
     static std::map<int,std::string> geneRegion::id2name;
     static std::set<int> geneRegion::allGeneId;
-    void addRegion(char *source);
-    void addRegion(int _chr,long long _st,long long _ed);
-    void solAndPrintRel(FILE *fp);
+
 };
-
-
-#endif //SRCFIND_GENEREGION_H
